a1b81b0d4b39b269e57e86d23614ca4e6fbd67b1
mantian <aaa.he@foxmail.com>
2020-09-26 20:27:52 +0800

    vue-elementui-2#0926 1.制作首页头部信息 2.制作卡片的布局

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,7 +2,17 @@
   <el-row class="home" :gutter="20">
     <el-col :span="8">
       <el-card shadow="hover" class="avatar">
-        鼠标悬浮时显示
+        <div class="avatar-info">
+          <img :src="avatar" />
+          <div class="avatar-desc">
+            <p class="name">jiang漫天</p>
+            <p>超级管理员</p>
+          </div>
+        </div>
+        <div class="avatar-item-info">
+          <p class="content">上次登录时间:<span>2020-09-26</span></p>
+          <p class="content">上次登录地点:<span>上海</span></p>
+        </div>
       </el-card>
       <el-card shadow="hover" class="info">
         鼠标悬浮时显示
@@ -10,11 +20,11 @@
     </el-col>
     <el-col :span="16">
       <div class="num">
-        <el-card shadow="hover" v-for="item in 6" :key="item">
-          <i class=""></i>
+        <el-card shadow="hover" v-for="item in iconList" :key="item.icon" class="card-num">
+          <i :class="`el-icon-${item.icon} icon`" :style="{ background: item.background, color: item.color }"></i>
           <div class="card-info">
-            <p class="money">＄ 1234</p>
-            <p class="info-item">今日支付订单</p>
+            <p class="money">{{ item.money }}</p>
+            <p class="info-item">{{ item.desc }}</p>
           </div>
         </el-card>
       </div>
@@ -38,6 +48,19 @@
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      avatar: require('@/assets/img/user.jpg'),
+      iconList: [
+        { name: 'icon-1', icon: 'circle-check', money: '$1234', desc: '今日支付订单', color: 'white', background: '#33aef0' },
+        { name: 'icon-1', icon: 'circle-check', money: '$1234', desc: '今日支付订单', color: 'white', background: 'pink' },
+        { name: 'icon-1', icon: 'circle-check', money: '$1234', desc: '今日支付订单', color: 'white', background: '#33aef0' },
+        { name: 'icon-1', icon: 'circle-check', money: '$1234', desc: '今日支付订单', color: 'white', background: 'pink' },
+        { name: 'icon-1', icon: 'circle-check', money: '$1234', desc: '今日支付订单', color: 'white', background: '#33aef0' },
+        { name: 'icon-1', icon: 'circle-check', money: '$1234', desc: '今日支付订单', color: 'white', background: 'pink' }
+      ]
+    }
+  },
   mounted() {
     this.$http('/home/getHomeData').then(
       res => {
